ff68c34005be46be96274d31d7dad10bcf1584e5
DwangShers <02230281.cst@rub.edu.bt>
2023-08-28 22:13:58 +0600

    Modified file

--- Temperature_Converto.py
+++ Temperature_Converto.py
@@ -1,21 +1,24 @@
 Temp_Convo = "Converting Temperature Calculator."
 print(Temp_Convo)
 
-Temp = (input("Convert (C)elsius to Farenheit, (F)arenheit to Celsius, (E)xit."))
-print('Choose C or F or E')
+while True:
+    Temp= (input("Convert (C)elsius to Farenheit, (F)arenheit to Celsius, (E)xit. Choose one:")).upper()
 
-if Temp == 'C':
-    cel = float(input("Enter temperature in Celsius:"))
-    result = (cel*9/5)+32
-    print(result)
+    if Temp == 'C':
+        cel = float(input("Enter temperature in Celsius:"))
+        result = (cel*9/5)+32
+        print(f"The {cel} Celsius is {result} Farenheit.")
 
-elif Temp == "F":
-    fah = float(input("Enter temperature in Farenheit:"))
-    Result = (fah-32)*5/9
-    print(Result)
+    elif Temp == "F":
+        fah = float(input("Enter temperature in Farenheit:"))
+        Result = (fah-32)*5/9
+        print(f"The {fah} Farenheit is {Result} Celsius")
 
-elif Temp == "E":
-    print("Bye bye.")
+    elif Temp == "E":
+        print("Bye bye.")
+        break
 
-else:
-    print()
+    else:
+        print("Invalid input, please choose C,F or E:")
+        
+    
